bd06dda9129c5829e156459acbc52a18b851766c
Vinicius M. Barbosa <viniciusdemirandabarbosa@gmail.com>
2023-08-03 09:37:23 -0300

    Funções com parâmetro!

--- 06-funcoes.php
+++ 06-funcoes.php
@@ -41,5 +41,46 @@ function dadosCurso(){
 <p>Estamos no curso de <?=dadosCurso()?></p>
 <p><?=dadosCurso()?>é ministrado no Senac Penha.</p>
 
+<hr>
+
+<h2>Função com parâmetro (ou argumentos)</h2>
+<?php
+function soma($valor1,$valor2, $valor3 ){
+   return $total = $valor1 + $valor2 + $valor3;
+}
+?>
+
+<!-- Chamando e exibindo diretamente o retorno -->
+<p>Resultado 1:<?=soma(10,20,5)?></p>
+<p>Resultado 2:<?=soma(23,84,1000.75)?></p>
+<?php
+// Chamando e guardando o retorno
+$resultado3 = soma(500, 480, 700);
+
+?>
+<p>Resultado 3: <?=$resultado3?></p>
+<?php
+if($resultado3 > 100){?>
+<p>Vinicius foi demitido!</p>
+<?php
+}
+?>
+
+
+<!-- Usando uma função como parte de uma condição -->
+<?php if(soma(2, 3, 10) >= 10 ){?>
+    <p>Kaue reprovado!!</p>
+<?php } ?>
+
+<h3>Função com parâmetros opcionais </h3>
+<?php
+// Parâmetro opcional com um valor padrão
+function saudacao($mensagem, $pessoa = "Fulano(a)"){
+    return "Olá, $mensagem $pessoa!";
+}
+?>
+<p><?= saudacao("Bom dia","Melissa")?></p>
+<p><?= saudacao("Boa noite","Tanaka")?></p>
+<p><?= saudacao("Boa tarde")?></p>
 </body>
 </html>
