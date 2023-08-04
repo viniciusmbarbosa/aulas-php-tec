c6727f7afb8e502bab27f950dc56181b451ee3fd
Vinicius M. Barbosa <viniciusdemirandabarbosa@gmail.com>
2023-08-04 10:35:56 -0300

    termario

--- correcao.php
+++ correcao.php
@@ -4,6 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <style>
+        .aprovado{
+            color: blue;
+        }
+        .reprovado{
+            color: red;
+        }
+    </style>
 </head>
 <body>
     <h1>Exercício 05</h1>
@@ -11,25 +20,32 @@
 
     <?php
 
-    function calcularMedia($n1, $n2 ){
+    function calcularMedia(float $n1, float $n2 ):float{
         //Versão 1 (variável local)
        $media = ($n1 + $n2) / 2;
         return $media;
     }
 
+    function verificarSituacao(float $valorMedia):string {
+      /*  Sintaxe alternativa usando opador ternário ?: */
+        return $valorMedia >= 7 ? "aprovado" : "reprovado";
+      
+        /*  if($valorMedia >= 7){
+            return "Aprovado";
+        }else {
+            return "Reprovado";
+        } */
+    }
+
     $nota1 = 8;
     $nota2 = 7;
     $media = calcularMedia($nota1, $nota2);
-
-    if($media >= 7){
-        $situacao = "aprovado";
-    }else{
-        $situacao = "reprovado";
-    }
-
+    $situacao = verificarSituacao($media);
+    
+   
     ?>
 
-    <p><?=$media?></p>
-    <p><?=$situacao?></p>
+    <p ><?=$media?></p>
+    <p class="<?=$situacao?>"><?=$situacao?></p>
 </body>
 </html>
