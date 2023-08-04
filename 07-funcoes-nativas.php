74ddb428fb5f58219fe001e7b656c86e4de5f417
Vinicius M. Barbosa <viniciusdemirandabarbosa@gmail.com>
2023-08-04 11:57:56 -0300

    Array_unique()
    Função que retorna um novo array  com dados únicos.

--- 07-funcoes-nativas.php
+++ 07-funcoes-nativas.php
@@ -79,6 +79,26 @@ extract($aluno);
 </ul>
 
 <h3>array_sum</h3>
+<?php
+/* Função para somar os valores de um array */
+$total = array_sum($valores);
+
+?>
+<p>Soma dos valores: <?=$total?></p>
+
+<h3>Array_unique()</h3>
+
+<?php
+/*  Função que retorna um novo array  com dados únicos. */
+$produtos = [
+    "TV", "Notebook", "TV", "Geladeira", "Monitor", "Notebook", "Teclado", "Mouse"
+];
+
+$produtosUnicos = array_unique($produtos);
+
+?>
+<pre><?=var_dump($produtos)?></pre>
+<pre><?=var_dump($produtosUnicos)?></pre>
 
     <h2>Filtros</h2>
 
