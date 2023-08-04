fd36be1435645f072b328e9c47103e03937c2e30
Vinicius M. Barbosa <viniciusdemirandabarbosa@gmail.com>
2023-08-04 11:02:45 -0300

    Usando a função trim

---
+++ 07-funcoes-nativas.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funções nativas</title>
+</head>
+<body>
+    <h1>Exemplo de funções nativas</h1>
+    <hr>
+
+    <h2>String</h2>
+    <h3>trim() - Função que remove espaços antese depois de strings.</h3>
+    <?php
+    $nome = "       Vinícius deve 5 paçocas   ";
+    $nomeSemEspaco = trim($nome);
+
+    ?>
+    <pre><?=var_dump($nome)?></pre>
+    <pre><?=var_dump($nomeSemEspaco)?></pre>
+    <h2>Numérica</h2>
+    
+    <h2>Array</h2>
+
+    <h2>Filtros</h2>
+
+    <h2>Seguranças</h2>
+
+</body>
+</html>
