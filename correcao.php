0116dfdd32614e1dadd21cde5df0db106f8ab013
Vinicius M. Barbosa <viniciusdemirandabarbosa@gmail.com>
2023-08-04 09:41:01 -0300

    Correção super básica

--- correcao.php
+++ correcao.php
@@ -6,6 +6,23 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>Exercício 05</h1>
+    <hr>
+
+    <?php
+    $nota1 = 8;
+    $nota2 = 7;
+    $media = ($nota1 + $nota2) / 2;
+
+    if($media >= 7){
+        $situacao = "aprovado";
+    }else{
+        $situacao = "reprovado";
+    }
+
+    ?>
+
+    <p><?=$media?></p>
+    <p><?=$situacao?></p>
 </body>
 </html>
