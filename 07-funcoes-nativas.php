31d390252d6f771eb61f86a7a1d08663b0cdee7e
Vinicius M. Barbosa <viniciusdemirandabarbosa@gmail.com>
2023-08-04 11:38:54 -0300

    Implode
    Função que transforma array em string

--- 07-funcoes-nativas.php
+++ 07-funcoes-nativas.php
@@ -29,11 +29,39 @@
     <p><?=$fraseFeia?></p>
     <p><?=$fraseBonitinha?></p>
 
+    <h3>explode()</h3>
+    <?php
+    /* Função que transforma uma string em um array */
+
+    $linguagens = "HTML - CSS - JS - PHP";
+    $arrayLinguagens = explode(" - ", $linguagens);
+    ?>
+    <pre><?=var_dump($linguagens)?></pre>
+    <pre><?=var_dump($arrayLinguagens)?></pre>
 
     <h2>Numérica</h2>
+
+    <h3>min, max, round</h3>
+    <?php
+    $valores = [10, 5, 50, 1000, 275];
+    $valoresQualquer = 1259.75;
+    ?>
+    <p>Menor valor: <?=min($valores)?></p>
+    <p>Maior valor: <?=max($valores)?></p>
+    <p>Arredondado: <?=round($valoresQualquer)?></p>
     
     <h2>Array</h2>
 
+    <h3>Implode()</h3>
+    <?php
+    /* Função que transforma array em string */
+    $bandas = ["Rush", "Sleyer", "Dio"];
+    $textoBandas = Implode(" - ", $bandas);
+    ?>
+    <pre><?=var_dump($bandas)?></pre>
+    <pre><?=var_dump($textoBandas)?></pre>
+
+    
     <h2>Filtros</h2>
 
     <h2>Seguranças</h2>
