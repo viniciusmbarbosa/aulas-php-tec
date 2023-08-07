438543a6294a4bfbb8bdc14e9b20217e18fe762e
Vinicius M. Barbosa <viniciusdemirandabarbosa@gmail.com>
2023-08-07 08:56:52 -0300

    Sanitização

--- 07-funcoes-nativas.php
+++ 07-funcoes-nativas.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Funções nativas</title>
 </head>
+
 <body>
     <h1>Exemplo de funções nativas</h1>
     <hr>
@@ -16,18 +18,18 @@
     $nomeSemEspaco = trim($nome);
 
     ?>
-    <pre><?=var_dump($nome)?></pre>
-    <pre><?=var_dump($nomeSemEspaco)?></pre>
+    <pre><?= var_dump($nome) ?></pre>
+    <pre><?= var_dump($nomeSemEspaco) ?></pre>
 
     <h3>str_replace()</h3>
     <?php
     /* Funcão que permite suvstituição de string/caracteres. */
     $fraseFeia = "<p>Fulano é um bobão e xarope</p>";
-    $fraseBonitinha = str_replace(["xarope", "bobão"],"******", $fraseFeia);
+    $fraseBonitinha = str_replace(["xarope", "bobão"], "******", $fraseFeia);
 
     ?>
-    <p><?=$fraseFeia?></p>
-    <p><?=$fraseBonitinha?></p>
+    <p><?= $fraseFeia ?></p>
+    <p><?= $fraseBonitinha ?></p>
 
     <h3>explode()</h3>
     <?php
@@ -36,8 +38,8 @@
     $linguagens = "HTML - CSS - JS - PHP";
     $arrayLinguagens = explode(" - ", $linguagens);
     ?>
-    <pre><?=var_dump($linguagens)?></pre>
-    <pre><?=var_dump($arrayLinguagens)?></pre>
+    <pre><?= var_dump($linguagens) ?></pre>
+    <pre><?= var_dump($arrayLinguagens) ?></pre>
 
     <h2>Numérica</h2>
 
@@ -46,10 +48,10 @@
     $valores = [10, 5, 50, 1000, 275];
     $valoresQualquer = 1259.75;
     ?>
-    <p>Menor valor: <?=min($valores)?></p>
-    <p>Maior valor: <?=max($valores)?></p>
-    <p>Arredondado: <?=round($valoresQualquer)?></p>
-    
+    <p>Menor valor: <?= min($valores) ?></p>
+    <p>Maior valor: <?= max($valores) ?></p>
+    <p>Arredondado: <?= round($valoresQualquer) ?></p>
+
     <h2>Array</h2>
 
     <h3>Implode()</h3>
@@ -58,51 +60,86 @@
     $bandas = ["Rush", "Sleyer", "Dio"];
     $textoBandas = Implode(" - ", $bandas);
     ?>
-    <pre><?=var_dump($bandas)?></pre>
-    <pre><?=var_dump($textoBandas)?></pre>
+    <pre><?= var_dump($bandas) ?></pre>
+    <pre><?= var_dump($textoBandas) ?></pre>
 
     <h3>extract()</h3>
-<?php
+    <?php
     /*Função que extrai chaves associativas para variáveis  */
     $aluno = [
         "id" => 1,
         "nome" => "Chave do 8",
         "idade" => 25,
     ];
-extract($aluno);
-?>
+    extract($aluno);
+    ?>
 
-<ul>
-    <li>Nome:<?=$nome?></li>
-    <li>ID:<?=$id?></li>
-    <li>Idade: <?=$idade?></li>
-</ul>
+    <ul>
+        <li>Nome:<?= $nome ?></li>
+        <li>ID:<?= $id ?></li>
+        <li>Idade: <?= $idade ?></li>
+    </ul>
 
-<h3>array_sum</h3>
-<?php
-/* Função para somar os valores de um array */
-$total = array_sum($valores);
+    <h3>array_sum</h3>
+    <?php
+    /* Função para somar os valores de um array */
+    $total = array_sum($valores);
 
-?>
-<p>Soma dos valores: <?=$total?></p>
+    ?>
+    <p>Soma dos valores: <?= $total ?></p>
 
-<h3>Array_unique()</h3>
+    <h3>Array_unique()</h3>
 
-<?php
-/*  Função que retorna um novo array  com dados únicos. */
-$produtos = [
-    "TV", "Notebook", "TV", "Geladeira", "Monitor", "Notebook", "Teclado", "Mouse"
-];
+    <?php
+    /*  Função que retorna um novo array  com dados únicos. */
+    $produtos = [
+        "TV", "Notebook", "TV", "Geladeira", "Monitor", "Notebook", "Teclado", "Mouse"
+    ];
 
-$produtosUnicos = array_unique($produtos);
+    $produtosUnicos = array_unique($produtos);
 
-?>
-<pre><?=var_dump($produtos)?></pre>
-<pre><?=var_dump($produtosUnicos)?></pre>
+    ?>
+    <pre><?= var_dump($produtos) ?></pre>
+    <pre><?= var_dump($produtosUnicos) ?></pre>
 
     <h2>Filtros</h2>
+    <p>Recurso de análise e limpeza de dados aplicado através das funções <code>filter_var</code> e <code>filter-input()</code>.</p>
+    <h3>Validação</h3>
+    <?php
+    $email = "tiago.com.br"
+
+    /* Se o e-mail informado for inválido, ou seja, se não seguir o padrao geral de endereços de e-mail, a função abaixo retornará */
+    ?>
+    <pre>
+    <?= var_dump(filter_var($email, FILTER_VALIDATE_EMAIL)) ?>
+</pre>
+
+    <h3>Sanitização</h3>
+    <!-- Cross script -->
+    <?php
+    $ataque = "<script>
+    document.body. innerHTML = '<h1>Sou ráqui!! ahahha :c </h1>'
+    </script>";
+    // Execução sem sanitização (script é válidado)
+
+    //echo $ataque
+
+    $ataqueSanitizado = filter_var(
+        $ataque, FILTER_SANITIZE_SPECIAL_CHARS
+    );
+
+    //Execução com sanitização  (Script e anulado)
+    Echo $ataqueSanitizado;
+
+    ?>
+
+    <hr>
+    <h2>Segurança</h2>
+
+
 
     <h2>Seguranças</h2>
 
 </body>
+<!-- brute force -->
 </html>
