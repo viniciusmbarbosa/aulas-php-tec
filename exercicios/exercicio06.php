87b5a64924eb8fc715dd4d77f61322d380a321e3
Vinicius M. Barbosa <viniciusdemirandabarbosa@gmail.com>
2023-08-10 10:35:03 -0300

    Terminando código

--- exercicios/exercicio06.php
+++ exercicios/exercicio06.php
@@ -4,69 +4,59 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <title>Fomulário Em PHP</title>
     <style>
+        html{
+            font-size: 16px;
+        }
     </style>
 </head>
 
-<body>
+<body >
 
-    <h1>Formulário</h1>
-    <hr>
-
-
-    <?php
-    $fabricantes = ["-", "loja1", "loja2", "loja3"];
-    ?>
-
-
-    <h2>Cadastro</h2>
-    <div>
-        <form action="metodo-post.php" method="post">
-            <p>
-                <label for="nome">Nome:</label>
-                <input required name="nomeProduto" type="text"><br>
-            </p>
-
-            <p>
-
-                <label for="fabricante">Fabricante:</label>
-                <select name="fabricante" id="fabricante">
-                    <?php
-                    foreach ($fabricantes as $fabricante) { ?>
-                        <option value="<?= $fabricante; ?>"><?= $fabricante ?></option>
-
-                    <?php } ?>
-                </select>
-
-
-            </p>
-
-            <p>
-
-                <label for="preco">Preço</label>
-                <input required type="number" name="preco" id="preco" min="100" max="10000"><br>
-            </p>
-
-
-            <p>
-            <h3>Disponibilidade </h3>
-            <label for="">Sim</label>
-            <input type="radio" name="disponibilidade" id="sim">
-            <label for="opcao">Não</label>
-            <input type="radio" name="disponibilidade" id="nao"><br>
-            </p>
-
-            <p>
-
-                <label for="descricao">Descrição do produto </label><br>
-                <textarea name="descricao" id="descricao" cols="30" rows="10"></textarea>
-                <button type="submit" name="enviar">Enviar</button>
-            </p>
-            <?php
-
-            ?>
-        </form>
+    <div class="container ">
+        <h1 class="bg-primary text-white p-3 rounded">Formulário</h1>
+        <hr>
+        <?php
+        $fabricantes = ["-", "Samsung", "Motorola", "LG"];
+        ?>
+        <h2>Cadastro</h2>
+        <div class="mb-3 ">
+            <form class="mb-3" action="metodo-post.php" method="post">
+                <p>
+                    <label for="nome">Nome:</label>
+                    <input required class="form-control" name="nomeProduto" type="text"><br>
+                </p>
+                <p>
+                    <label for="fabricante">Fabricante:</label>
+                    <select name="fabricante" id="fabricante">
+                        <?php
+                        foreach ($fabricantes as $fabricante) { ?>
+                            <option value="<?= $fabricante; ?>"><?= $fabricante ?></option>
+                        <?php } ?>
+                    </select>
+                </p>
+                <p>
+                    <label for="preco">Preço: </label>
+                    <input class="form-control" required type="number" name="preco" id="preco" min="100" max="10000"><br>
+                </p>
+                <p>
+                <h3>Disponibilidade </h3>
+                <label for="">Sim</label>
+                <input value="Sim" type="radio" name="disponibilidade" id="sim">
+                <label for="opcao">Não</label>
+                <input value="Não" type="radio" name="disponibilidade" id="nao"><br>
+                </p>
+                <p>
+                    <label for="descricao" class="form-label">Descrição do produto </label><br>
+                    <textarea class="form-control row-3"  placeholder="Mensagem:" name="descricao" id="descricao" cols="30" rows="10"></textarea><br>
+                    <button type="submit" name="enviar">Enviar</button>
+                </p>
+                <?php
+                ?>
+            </form>
+        </div>
     </div>
 
 </body>
--- exercicios/metodo-post.php
+++ exercicios/metodo-post.php
@@ -6,15 +6,24 @@
     <title>Informações do formulário</title>
 </head>
 <body>
- <?php   
+ <?php  
+ 
+ if( empty($_POST["nomeProduto"]) || empty($_POST["preco"])) { ?>
+    <p>O produto e o nome precisam ser preenchido!!</p>
+    <a href="exercicio06.php">Voltar</a>
+<?php } else{
+
 if (isset($_POST["enviar"])){
     $nomeProduto = filter_input(INPUT_POST, "nomeProduto", FILTER_SANITIZE_SPECIAL_CHARS);
     $preco = filter_input(INPUT_POST, "preco", FILTER_SANITIZE_NUMBER_INT);
     $disponibilidade = filter_input(INPUT_POST, "disponibilidade", FILTER_SANITIZE_SPECIAL_CHARS);
-    $descricao = filter_input(INPUT_POST, "descricao", FILTER_SANITIZE_NUMBER_INT);
+    $descricao = filter_input(INPUT_POST, "descricao", FILTER_SANITIZE_SPECIAL_CHARS);
     $fabricante = filter_input(INPUT_POST, "fabricante", FILTER_SANITIZE_SPECIAL_CHARS);
 
+    
 }
+    
+
 ?>
 
 <h1>Cadastro de produtos</h1>
@@ -26,5 +35,10 @@ if (isset($_POST["enviar"])){
     <li>Disponibilidade: <?=$disponibilidade?></li>
     <li>Descrição:<?=$descricao?></li>
 </ul>
+<?php
+
+} 
+
+?>
 </body>
 </html>
